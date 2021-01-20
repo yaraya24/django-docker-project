533044f976137fb24b4a15259798976aa11ac576
Yonnas Araya <yonnas.a24@hotmail.com>
2021-01-20 21:12:28 +1100

    basic unit testing and views

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_homepage_status_code(self):
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_homepage_contains_correct_html(self):
+        self.assertContains(response, 'Homepage')
+
+    def test_homepage_does_not_contain_incorrect_html(self):
+        self.assertNotContains(response, 'hi there')
+
+    def test_homepage_url_resolve_homepageview(self):
+        view = resolve('/')
+        self.assertEqual(
+            view.func.__name__,
+            HomePageView.as_view().__name__
+        )
