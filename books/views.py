9598b6123e2e770fb37639d4b0d3f67b5c2f8520
Yonnas Araya <yonnas.a24@hotmail.com>
2021-02-02 18:58:40 +1100

    added search feature to books by author and title

--- books/views.py
+++ books/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 
 from .models import Book
 
-class BookListView(ListView):
+class BookListView(LoginRequiredMixin, ListView):
     model = Book
     context_object_name = "book_list" # refers to the objects of the model
     template_name = 'books/book_list.html'
+    login_url = 'account_login'
 
-class BookDetailView(DetailView):
+class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
     model = Book
     context_object_name = 'book'
     template_name = 'books/book_detail.html'
+    login_url = 'account_login'
+    permission_required = 'books.special_status'
 
+class SearchResultsListView(ListView):
+    model = Book
+    context_object_name = "book_list"
+    template_name = 'books/search_results.html'
+    
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        return Book.objects.filter(
+            Q(title__icontains=query) | Q(author__icontains=query)
+        )
